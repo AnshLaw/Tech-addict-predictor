bbf7dfd350bf66649ecc7e34be3c3515a7d7b26c
Ansh R. Suryavanshi <110959972+AnshLaw@users.noreply.github.com>
2024-07-05 16:01:57 -0400

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -3,9 +3,9 @@
 # Install system-level dependencies
 if command -v apt-get >/dev/null; then
     sudo apt-get update
-    sudo apt-get install -y python3-tk xvfb
+    sudo apt-get install -y python3-tk xvfb xauth
 elif command -v dnf >/dev/null; then
-    sudo dnf install -y python3-tkinter xvfb
+    sudo dnf install -y python3-tkinter xorg-x11-server-Xvfb xorg-x11-xauth
 fi
 
 # Upgrade pip, setuptools, and wheel
