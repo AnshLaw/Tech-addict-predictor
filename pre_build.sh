abdb5de8ef64322fed7c179f51c1b8060652fde0
Ansh R. Suryavanshi <110959972+AnshLaw@users.noreply.github.com>
2024-07-05 15:43:57 -0400

    Update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip setuptools wheel
-pip install numpy==1.21.2  # Ensure numpy is installed first
+pip install numpy==1.20.3  # Adjust the numpy version to be compatible
 pip install scikit-learn==0.24.2 --no-use-pep517
